123b9813ae58783ca9c0666e8712d4380656367a
GrowthAG <gsidegroup@gmail.com>
2025-05-15 15:39:26 -0300

    Update landing-page.php

--- landing-page.php
+++ landing-page.php
@@ -1,8 +1,18 @@
 <?php
 /* Template Name: Landing Security First */
-get_header();
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php the_title(); ?></title>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
 
-<div id="root"></div>
+  <div id="root"></div>
 
-<?php get_footer(); ?>
+  <?php wp_footer(); ?>
+</body>
+</html>
